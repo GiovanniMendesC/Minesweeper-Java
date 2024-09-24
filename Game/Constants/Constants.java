87a6f823c4c7f8d4c98e0e0304c717d0c43410ec
Giovanni Mendes <gmendes@tnsi.com>
2024-09-24 17:53:26 -0300

    primeiro commit

---
+++ Game/Constants/Constants.java
@@ -0,0 +1,13 @@
+package Game.Constants;
+
+public class Constants {
+    //Game
+    public static final int MAX_BOMBAS = 128;
+    public static final int MAX_ESPACO_LIBERAR = 20;
+    public static final int MIN_ESPACO_LIBERAR = 10;
+    public static final int RAIO_ESPACO_LIBERAR = 4;
+
+    //Mapa
+    public static final int[] POSICAO_INCIAL = { 9, 9 };
+
+}
